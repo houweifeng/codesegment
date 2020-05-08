7b5a3bb83047f1f43885dcd5b3f1f49dcad7ecc6
lich0 <lichao.890427@163.com>
2020-05-08 15:36:36 +0800

    Add files via upload

---
+++ app2ipa.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# 该模块在iOS上将App所在Bundle目录打包为IPA文件
+
+if [[ x$1 == x ]]; then
+    echo Usage: $0 [apppath]
+    exit 0
+fi
+
+apppath=$(echo /private/var/containers/Bundle/Application/*/$1.app)
+
+if [ ! -d $apppath ]; then
+    apppath=$(echo /private/var/mobile/Containers/Bundle/Application/*/$1.app)
+    if [ ! -d $apppath ]; then
+        echo app not found
+        exit
+    fi
+fi
+
+spliter=(${apppath//// })
+appbase=${spliter[${#spliter[*]}-1]}
+appname=${appbase//.app/}
+
+echo Appname is $appname
+
+rm -rf /tmp/$appname
+mkdir -p /tmp/$appname/Payload
+cp -rfp $apppath /tmp/$appname/Payload/
+if [ -f $apppath/../iTunesArtwork ]; then
+    cp -fp $apppath/../iTunesArtwork /tmp/$appname/iTunesArtwork
+elif [ -f $apppath/Icon.png ]; then
+    cp -fp $apppath/Icon.png /tmp/$appname/iTunesArtwork
+elif [ -f $apppath/Default.png ]; then
+    cp -fp $apppath/Default.png /tmp/$appname/iTunesArtwork
+fi
+olddir=$PWD
+cd /tmp/$appname
+zip -r $olddir/$appname.ipa Payload
+cd $olddir
+rm -rf /tmp/$appname
+
+echo IPA package create at $olddir/$appname.ipa
